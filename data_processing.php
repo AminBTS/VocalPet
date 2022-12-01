e47c94b6c4b974cc5706e09eb4516c5686e67818
Saekou <119595740+Saekou@users.noreply.github.com>
2022-12-01 13:03:06 +0100

    Update data_processing.php

--- data_processing.php
+++ data_processing.php
@@ -1,1 +1,29 @@
-futur php
+<?php 
+$bdd = mysqli_connect('localhost', 'amindb', 'azerty', 'saeko');
+$prenom = $_POST['prenom'];
+$nom = $_POST['nom'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$adresse = $_POST['adresse'];
+$ville = $_POST['ville'];
+$postal = $_POST['postal'];
+$telephone = $_POST['telephone'];
+$pseudo = $_POST['pseudo'];
+$action = $_POST['action'];
+
+// etc
+
+switch($action) {
+  case 'inscription':
+    require('account.html');
+    $query = "INSERT INTO utilisateurs (prenom, nom, password, email, adresse, ville, postal, telephone, pseudo) 
+    VALUES ('$prenom', '$nom', '$password', '$email', '$adresse', '$ville', '$postal', '$telephone', '$pseudo')";
+    mysqli_query($bdd, $query)
+    break;
+
+  case 'connexion':
+    //code
+   break;
+}
+
+?>
